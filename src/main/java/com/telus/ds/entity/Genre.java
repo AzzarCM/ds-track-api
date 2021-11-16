b9598be9bedc688bed077fac7ca8b6531f95bbf8
AxelQuintanilla <axel.quintanilla620@gmail.com>
2021-11-16 16:49:26 -0600

    Genre controller, service, repository, entity and DTO

---
+++ src/main/java/com/telus/ds/entity/Genre.java
@@ -0,0 +1,50 @@
+package com.telus.ds.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+/**
+ *
+ * @author axel.hernandez02
+ */
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "GENRE")
+public class Genre {
+    public Genre() {}
+	
+	public Genre(Integer genre_id, String genre_name) {
+		super();
+		this.genre_id = genre_id;
+		this.genre_name = genre_name;
+	}
+        
+        @Id
+	@Column(name="GENRE_ID", updatable=false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer genre_id;
+	
+	@Column(name="GENRE_NAME", updatable=false)
+	@Length(max = 12, min = 12)
+	@NotNull(message = "Genre Name is required")
+	private String genre_name;
+        
+        @Column(name="CREATION_DATE", updatable=false)
+	@NotNull(message = "CREATION DATE is required")
+	private LocalDateTime creationDate;
+}
